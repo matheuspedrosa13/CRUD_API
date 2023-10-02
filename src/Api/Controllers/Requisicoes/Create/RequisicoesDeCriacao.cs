49228716963f5996ea24342577365ab06c1c36c3
matheuspedrosa13 <matheuspmc13@gmail.com>
2023-10-02 11:37:06 -0300

    feat: adicionando pastas para requisições do CRUD

---
+++ src/Api/Controllers/Requisicoes/Create/RequisicoesDeCriacao.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudOfertas.Api.Controllers.Requisicoes.Create;
+public class RequisicoesDeCriacao
+{
+    
+}
